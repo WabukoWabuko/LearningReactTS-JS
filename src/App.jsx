3f4a37481df2721fb94e8374c21da5cdb91a759f
WabukoWabuko <basilwabbs@gmail.com>
2025-01-13 23:32:08 +0300

    Started React and Vite Learning

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MyName from "./MyName"
+
+function App() {
+
+  return (
+    <>
+      <h1>React and Vite kickstart</h1>
+      <MyName/>
+    </>
+  )
+}
+
+export default App
---
+++ src/MyName.jsx
@@ -0,0 +1,7 @@
+function MyName() {
+    return (
+        <h1>My Name is Wabuko</h1>
+    )
+}
+
+export default MyName
