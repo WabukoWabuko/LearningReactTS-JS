c9c6f9ab767f7291e0286d82c3abd5b4058963dd
WabukoWabuko <basilwabbs@gmail.com>
2025-01-16 10:26:07 +0300

    Using conditional statements in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,46 @@
-// import MyName from "./MyName"
-import Gallery from "./Gallery"
-import { Profile } from "./Gallery"
+// // import MyName from "./MyName"
+// import Gallery from "./Gallery"
+// import { Profile } from "./Gallery"
 
-export default function App() {
+// export default function App() {
 
-  return (
-    <>
-      <Gallery/>
-      <Profile/>
-      {/* <h1>React and Vite kickstart</h1>
-      <h1>Officially Starting on a serious note.</h1>
-      <MyName name="Trendafilova"/> */}
-    </>
-  )
+//   return (
+//     <>
+//       <Gallery/>
+//       <Profile/>
+//       {/* <h1>React and Vite kickstart</h1>
+//       <h1>Officially Starting on a serious note.</h1>
+//       <MyName name="Trendafilova"/> */}
+//     </>
+//   )
+// }
+
+// // export default App
+
+
+function Item({ name, isPacked }) {
+  if (isPacked){return <li className="items">{name} ✅</li>;}
+  return <li className="items">{name}</li>;
 }
 
-// export default App
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item 
+          isPacked={true} 
+          name="Space suit" 
+        />
+        <Item 
+          isPacked={true} 
+          name="Helmet with a golden leaf" 
+        />
+        <Item 
+          isPacked={false} 
+          name="Photo of Tam" 
+        />
+      </ul>
+    </section>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <section>
-    <h1>These are amazing  scientists</h1>
+    {/* <h1>These are amazing  scientists</h1> */}
     <App/>
     </section>
     
