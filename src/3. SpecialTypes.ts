5552c7ce475ea805924d32744a4b56ac2a35e728
WabukoWabuko <basilwabbs@gmail.com>
2025-08-16 12:06:03 +0300

    Using Type: any

---
+++ src/3. SpecialTypes.ts
@@ -0,0 +1,15 @@
+// Type: any
+// This type disables errors because the parsed value can be of any type.
+// But for the Example below it will throw errors because of not specifying the TYPE: any
+
+// let u = true;
+// u = "string";
+
+// Math.round(u);
+
+// Using Type: any
+
+let t: any =  true;
+t = "string";
+
+Math.round(t); // No error is shown/thrown
