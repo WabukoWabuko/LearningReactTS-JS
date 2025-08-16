5fe997d5efe31dac8f5fbe803a7f1ad8cae3b23d
WabukoWabuko <basilwabbs@gmail.com>
2025-08-16 12:25:11 +0300

    Used keyword 'readonly' for protecting arrays from being edited

---
+++ src/4. Arrays.ts
@@ -0,0 +1,10 @@
+// const names: string[] = [];
+
+// names.push("Adams");
+
+// console.log(names)
+
+// // Readonly Keyword that prevents an Arrays from being changed
+// const unit: readonly string[] = ["Lambert"];
+// unit.push("Jack") // An error is raised showing that keyword push is not applied in readonly format.
+
