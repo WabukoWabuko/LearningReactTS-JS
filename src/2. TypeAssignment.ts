fb16b8606bc547307c3a576b34ddad5313167ebb
WabukoWabuko <basilwabbs@gmail.com>
2025-08-16 11:58:12 +0300

    Infering the Type of the value given

--- src/2. TypeAssignment.ts
+++ src/2. TypeAssignment.ts
@@ -1,21 +1,25 @@
 // Explicit Type
 
-let firstName: string = "WabukoTrendafilova";
+// let firstName: string = "WabukoTrendafilova";
 
-console.log(firstName)
+// console.log(firstName)
 
-// Implicit Type
+// // Implicit Type
 
-let secondName = "Ribashongilo";
+// let secondName = "Ribashongilo";
 
-console.log(secondName)
+// console.log(secondName)
 
 // Example of type change in Explicit Type
-let unit: string = "Automata Theory";
+// let unit: string = "Automata Theory";
 
-unit = 45; // This one has underlined the type change as an error
+// unit = 45; // This one has underlined the type change as an error
 
-// Example of type change in Implicit Type 
-let Tunit = "Machine Learning";
+// // Example of type change in Implicit Type 
+// let Tunit = "Machine Learning";
 
-Tunit = 12; // Has also shown the error. 
+// Tunit = 12; // Has also shown the error. 
+
+const json = JSON.parse("55");
+
+console.log(typeof json)
