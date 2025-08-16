61f14ff2cbfdeb664ae21ecf9c7c418ede71137c
WabukoWabuko <basilwabbs@gmail.com>
2025-08-16 11:54:40 +0300

    Type checking to see if we can assign a different type to a the one declared  at first.

--- src/2. TypeAssignment.ts
+++ src/2. TypeAssignment.ts
@@ -1,5 +1,21 @@
 // Explicit Type
 
-let firstName: string = "WabukoAdams";
+let firstName: string = "WabukoTrendafilova";
 
 console.log(firstName)
+
+// Implicit Type
+
+let secondName = "Ribashongilo";
+
+console.log(secondName)
+
+// Example of type change in Explicit Type
+let unit: string = "Automata Theory";
+
+unit = 45; // This one has underlined the type change as an error
+
+// Example of type change in Implicit Type 
+let Tunit = "Machine Learning";
+
+Tunit = 12; // Has also shown the error. 
